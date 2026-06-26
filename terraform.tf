b7df3008326b04c35f34f2ffb27dd8d792c49bbb
vishal-avyka <vishal.b.taware@avyka.com>
2026-06-26 10:58:37 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,5 +1,12 @@
 terraform {
-  backend "local" {}
+  backend "http" {
+    address = "https://app.harness.io/gateway/iacm/api/orgs/playground/projects/vishal_playground/workspaces/Harness_Iacm_Learning_Prob_2/terraform-backend?accountIdentifier=gLXLxjTmQhGmdJXhAORKag"
+    username = "harness"
+    lock_address = "https://app.harness.io/gateway/iacm/api/orgs/playground/projects/vishal_playground/workspaces/Harness_Iacm_Learning_Prob_2/terraform-backend/lock?accountIdentifier=gLXLxjTmQhGmdJXhAORKag"
+    lock_method = "POST"
+    unlock_address = "https://app.harness.io/gateway/iacm/api/orgs/playground/projects/vishal_playground/workspaces/Harness_Iacm_Learning_Prob_2/terraform-backend/lock?accountIdentifier=gLXLxjTmQhGmdJXhAORKag"
+    unlock_method = "DELETE"
+  }
 
   required_providers {
     azurerm = {
